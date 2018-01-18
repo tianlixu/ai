afd4769b6d07dbe28f35167f849bbb7be4928a9e
Alex Xu <tianli.xu@gmail.com>
2018-01-18 00:10:57 -0500

    solution for the predition of nn

--- ml-ng/ex3-neural-networks/ex3/predict.m
+++ ml-ng/ex3-neural-networks/ex3/predict.m
@@ -21,13 +21,19 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+% add x0
+X = [ones(m, 1) X];
 
+Z2 = X * Theta1';
+A2 = sigmoid(Z2);
 
+% add a0
+A2 = [ones(m, 1) A2];
+Z3 = A2 * Theta2';
+A3 = sigmoid(Z3);
 
-
-
-
-
+[values, col] = max(A3, [], 2)
+p = col
 
 % =========================================================================
 
